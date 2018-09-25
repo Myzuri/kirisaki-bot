acde546f67a7eb2abccbcc4704345b2dc7ebb991
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-09-25 22:05:18 +0200

    Delete changenick.js

--- command/changenick.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (Discord, client, args, message, nick) => {
-
-
-
-    client.user.setUsername(nick) //.then(user => message.channel.sendMessage(`My new nickname is ${user.username}!`)).catch(console.error);
-}
