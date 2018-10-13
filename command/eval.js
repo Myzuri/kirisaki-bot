115f996daed03337c5ca5f5c11ee880e56b33174
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-13 18:16:11 +0200

    Update eval.js

--- command/eval.js
+++ command/eval.js
@@ -1,19 +1,4 @@
-
 module.exports.run = async (Discord, client, message, args) =>{
-
-    if(message.author.id !== '446738422346809354' ) return;//this is my own id, i can use process.env but i prefer let it there
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
   
   function clean(text) {
     if (typeof(text) === "string")
@@ -21,3 +6,17 @@ module.exports.run = async (Discord, client, message, args) =>{
     else
         return text;
   }
+
+      if(message.author.id !== "378879176515780619") return;
+      try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+   
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+   
+        message.channel.send(clean(evaled), {code:"xl"});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      }
+    };
