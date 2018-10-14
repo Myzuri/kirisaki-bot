4159e53e033faccefd10ace82d400aaa50077072
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-14 11:37:38 +0200

    Update dsay.js

--- command/dsay.js
+++ command/dsay.js
@@ -1,10 +1,10 @@
 module.exports.run = async (Discord, client, message, args) =>  {
 
-	var say = args.join(' ')
+	var say = args.join(' ')//catch msg arguments 
 
-	if(!say [0] || say[0 == 'help']) return message.channel.send("```!dsay [content] \n cannot be empty | don't even try !dsay [!sday]```")
-	
-		message.delete(1000)
-		return message.channel.send(say)
+	if(!say [0] || say[0 == 'help']) return message.channel.send("```!dsay [content] \n cannot be empty | don't even try !dsay [!dsay]```")
+	//security if msg length is empty or contain help
+		message.delete(1000)//delete your msg 
+		return message.channel.send(say)//bot send your msg
 	
 }
