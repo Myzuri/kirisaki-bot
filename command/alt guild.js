d88ac48075f3b35d48cd52ea220c90f6c907a209
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-18 00:46:31 +0200

    Create alt guild.js

---
+++ command/alt guild.js
@@ -0,0 +1,12 @@
+
+module.exports.run = async (Discord, client, message, args) =>{
+
+    if(message.author.id !== '378879176515780619') return;
+    var test = client.guilds.map(g => `${g.name} ID | ${g.id}`).join(', ');
+
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .addField('List of server', `${test}`)
+        message.channel.send(embed)
+
+}
