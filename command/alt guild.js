4678a06976f43be38f09ca2df5e7737e6c0594fc
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-18 00:47:50 +0200

    Update alt guild.js

--- command/alt guild.js
+++ command/alt guild.js
@@ -1,4 +1,4 @@
-
+//it's the as the guild commande but if you don't like how the other command look you can use this instead
 module.exports.run = async (Discord, client, message, args) =>{
 
     if(message.author.id !== '378879176515780619') return;
