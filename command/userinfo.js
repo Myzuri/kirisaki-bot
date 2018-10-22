f8ef562f2673b7688c307dafcc7c334cef7d0a7c
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-22 21:17:57 +0200

    Update userinfo.js

--- command/userinfo.js
+++ command/userinfo.js
@@ -5,13 +5,12 @@ module.exports.run = async (client, args, message) => {
       let user = message.mentions.users.first() || message.author;
       let embed = new Discord.RichEmbed()
       .setColor('RANDOM')
-      .setTitle("Voici les informations du compte " + `${user.username}`)
+      .setTitle("Here the account info's " + `${user.username}`)
       .setThumbnail(user.displayAvatarURL)
-      .addField("Etat", user.presence.status, true)
+      .addField("State", user.presence.status, true)
       .addField("ID", user.id, true)
       .setDescription('[full image]('+user.avatarURL+')')
-      //.addField("Joue a ", "\`" + user.presence.game.name + "\`")
-      .setFooter("Compte créer le : ")
+      .setFooter("account created at ")
       .setTimestamp(user.createdAt)
       return message.channel.send(embed)
 
