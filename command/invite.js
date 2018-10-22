c1194808be93545da87f65e6820831ebf7596e96
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-22 11:44:16 +0200

    Update invite.js

--- command/invite.js
+++ command/invite.js
@@ -7,6 +7,6 @@ module.exports.run = async (Discord, client, message) => {
     .setDescription(`Here there's all the stuff to invite and get some help ${user.username}`  )
     .setThumbnail(client.user.displayAvatarURL)
     .addField("My server", "[Just right here](https://discord.gg/W7vm4WY)")
-    .addField('Invite me', "[invitation to your server](https://discordapp.com/oauth2/authorize?&client_id=471999952642375680&scope=bot&permissions=8)")
+    .addField('Invite me', `[invitation to your server](https://discordapp.com/oauth2/authorize?&client_id=${client.user.id}&scope=bot&permissions=8)`)
     return message.channel.send(embed)
 }
