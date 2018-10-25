b5a5dd732ce4d22e16a3d89b6783eb2a852887d8
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-25 14:02:57 +0200

    Update calc.js

--- command/calc.js
+++ command/calc.js
@@ -4,7 +4,14 @@ module.exports.run = async (Discord, client, message, args) => {
 	let calcul = args.join(' ');
 try{
 	let mathc = math.eval(calcul);
-	message.channel.send(mathc)
+
+	let embed = new Discord.RichEmbed()
+	.setColor('RANDOM')
+	.setAuthor(message.author.tag, message.author.avatarURL)
+	.setTitle('I\'m enough smart to know the result UwU')
+	.addField('Input', `\`\`\`${calcul}\`\`\``)
+	.addField('Output', `\`\`\`${mathc}\`\`\``)
+	message.channel.send(embed)
 } catch (error) {
 	message.channel.send("It won't work")
 }
