f1d50bd18eee877b5326c2fedfd23ca638998fa1
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-10-25 14:57:12 +0200

    Update leave.js

--- command/leave.js
+++ command/leave.js
@@ -1,10 +1,10 @@
 module.exports.run = async (Discord, client, message, args, ytdl, streamOptions) => {
 
-        if (!message.member.voiceChannel) return message.channel.send("T'es pas connecté a un channel vocal");
+        if (!message.member.voiceChannel) return message.channel.send("You're not connected to vocal channel");
 
-        if (!message.guild.me.voiceChannel) return message.channel.send("Je suis pas connecté :neutral_face");
+        if (!message.guild.me.voiceChannel) return message.channel.send("I'm not connected");
 
-        if (message.guild.me.voiceChannelID !== message.guild.me.voiceChannelID) return message.channel.send("T'es pas connecté dans le même channel que moi");
+        if (message.guild.me.voiceChannelID !== message.guild.me.voiceChannelID) return message.channel.send("You're not in the same channel ¯\_(ツ)_/¯");
        
         message.guild.me.voiceChannel.leave()
        
