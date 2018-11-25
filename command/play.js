4265c5e6c638065ed06224a82a22ac59d51552a8
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2018-11-25 11:49:25 +0100

    Update play.js

--- command/play.js
+++ command/play.js
@@ -1,7 +1,11 @@
 module.exports.run = async (Discord, client, message, args, ytdl, streamOptions) => {
 
-    var [link] = args;
-  
+    var link = args[0];
+    
+    let valide_url = await ytdl.validateURL(link);
+    if(!valide_url) return message.channel.send('Input a **VALID** url')
+    
+    let info = await ytdl.getInfo(link)
     message.member.voiceChannel.join()
   .then(connection => {
     console.log('play');
@@ -11,7 +15,7 @@ module.exports.run = async (Discord, client, message, args, ytdl, streamOptions)
     let embed = new Discord.RichEmbed()
     .setColor('RANDOM')
     .setAuthor(`${message.author.username}`)
-    .addField('Actually playing', link)
+    .addField('Now playing', `${info.title}`)
     return message.channel.send(embed)
   })
   .catch(console.error);
