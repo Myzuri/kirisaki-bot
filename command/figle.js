4c7fa04d337a27c2d6b5a25fb04344293865c4d9
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2019-01-05 23:31:17 +0100

    Update figle.js

--- command/figle.js
+++ command/figle.js
@@ -1,11 +1,11 @@
 const figlet = require('figlet');
 module.exports.run = async (Discord, client, message, args) => {
 
-        let text = args.join(' ');
+        let texte = args.join(' ');
 
 
-        if (!text) return message.channel.send("```!ascii [text]\n ---------------------\n let you transform a short text into ascii```");
-        figlet(text, function(err, data) {
+        if (!texte) return message.channel.send("```!ascii [text]\n ---------------------\n let you transform a short text into ascii```");
+        figlet.text(texte, {font: 'Ghost'},function(err, data) {
                 if (err) {
                     console.log('Something went wrong...');
                     console.dir(err);
