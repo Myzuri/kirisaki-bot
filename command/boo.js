434c2433d346720033f5a8af8595f26484a0e3b3
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2019-01-07 12:39:07 +0100

    Update boo.js

--- command/boo.js
+++ command/boo.js
@@ -4,7 +4,7 @@ module.exports.run = async (Discord, client, message, args) => {
         let texte = args.join(' ');
 
 
-        if (!texte) return message.channel.send("```!ascii [text]\n ---------------------\n let you transform a short text into ascii```");
+        if (!texte) return message.channel.send("```!boo [text]\n ---------------------\n let you transform a short text into boo font OwO```");
         figlet.text(texte, {font: 'Ghost'},function(err, data) {
                 if (err) {
                     console.log('Something went wrong...');
