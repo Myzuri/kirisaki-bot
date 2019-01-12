b136cbef4b2c5e8866b37fabeb94ad8c97f9eacb
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2019-01-12 18:14:08 +0100

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -14,7 +14,7 @@ module.exports.run = async (Discord, client, message, args) => {
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
 			.setDescription("Avatar de " + `${author.username}`)
-			.setImage(`${user.displayAvatarURL}?size=1024`)
+			.setImage(`${user.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 
 		} if (tags) {
@@ -23,14 +23,14 @@ module.exports.run = async (Discord, client, message, args) => {
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
 			.setDescription("Avatar de " + `${author.username}`)
-			.setImage(`${tags.displayAvatarURL}?size=1024`)
+			.setImage(`${tags.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 		} else {
 			var author = message.author;
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
 			.setDescription("Votre avatar")
-			.setImage(`${author.displayAvatarURL}?size=1024`)
+			.setImage(`${author.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${message.author.tag} used avatar`))
 		
 		} 
