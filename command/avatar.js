cb3ecb33088d6d2e5e50524f0a845e554a62232b
Yu-shii <42755115+Yu-shii@users.noreply.github.com>
2019-01-12 15:55:34 +0100

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -1,37 +1,40 @@
 
+
 module.exports.run = async (Discord, client, message, args) => {
 
-	let user = message.mentions.users.first();//get the first mention
-	let search = args.join(' ');//search an user if no one is mention
-	let tags = client.users.find(u => u.username === search);//find the user (work with username only)
-	var author; // mention to get avatar
+	let avuser = message.author;
+	let user = message.mentions.users.first();
+	let search = args.join(' ');
+	let tags = client.users.find(u => u.username === search);
+	var author; // mention to get avatar, or type username
 	
-		if(user){//work if a mention is used
+		if(user){
 			var author = user;
 
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
-			.setDescription(`${author.tag}'s avatar`)
-			.setImage(user.displayAvatarURL)
-			return message.channel.send(embed)
+			.setDescription("Avatar de " + `${author.username}`)
+			.setImage(`${user.displayAvatarURL}?size=1024`)
+			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 
-		} if (tags) {//work if a username is "search"
+		} if (tags) {
 			var author = tags;
 
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
-			.setDescription(`${author.tag}'s avatar`)
-			.setImage(tags.displayAvatarURL)
-			return message.channel.send(embed)
-			
-		} else {//show your own avatar (!avatar)
+			.setDescription("Avatar de " + `${author.username}`)
+			.setImage(`${tags.displayAvatarURL}?size=1024`)
+			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
+		} else {
 			var author = message.author;
-			
 			let embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
-			.setDescription("Your avatar")
-			.setImage(author.displayAvatarURL)
-			return message.channel.send(embed)
+			.setDescription("Votre avatar")
+			.setImage(`${author.displayAvatarURL}?size=1024`)
+			return message.channel.send(embed).then(() => console.log(`${message.author.tag} used avatar`))
+		
 		} 
+		
+		
 
 }
